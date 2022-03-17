feb76ffe6e0ba39c604b5a30034dfad709404468
Uladzislau Dauhashei <v.dolgoshey@gmail.com>
2022-03-17 18:59:40 +0100

    added db connection

--- src/modules/MainModule/main.module.ts
+++ src/modules/MainModule/main.module.ts
@@ -1,5 +1,17 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+    imports: [
+        ConfigModule.forRoot(),
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: async (configService: ConfigService) => ({
+                uri: configService.get<string>("MONGODB_URI")
+            })
+        })
+    ]
 })
-export class MainModule {}
+export class MainModule { }
